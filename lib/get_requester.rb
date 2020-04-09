527243424078802545c6a62e1f389bf6225434c3
Mariam Haider <mfh.haider@gmail.com>
2020-04-09 21:57:22 +0000

    Automatically backed up by Learn

--- lib/get_requester.rb
+++ lib/get_requester.rb
@@ -7,7 +7,7 @@ class GetRequester
   URL = "https://learn-co-curriculum.github.io/json-site-example/endpoints/people.json"
   
   def get_response_body 
-  #uri = URI.parse(URL)
+    uri = URI.parse(URL)
   #response = Net::HTTP.get_response_body(uri)
   #response.body
   
