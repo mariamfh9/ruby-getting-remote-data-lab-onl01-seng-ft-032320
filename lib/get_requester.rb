1f049314cb6b032add7065e72966da1086bc9519
Mariam Haider <mfh.haider@gmail.com>
2020-04-09 21:32:46 +0000

    Automatically backed up by Learn

--- lib/get_requester.rb
+++ lib/get_requester.rb
@@ -6,7 +6,7 @@ class GetRequester
   URL = "http://data.cityofnewyork.us/resource/uvks-tn5n.json"
   
   def get_response_body 
-    url = "https://learn-co-curriculum.github.io/json-site-example/"
+    
   end 
   
   def parse_json
