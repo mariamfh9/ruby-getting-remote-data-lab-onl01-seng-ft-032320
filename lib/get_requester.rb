f44f31b8eeccf581f52223377fe8dd46894e3c9f
Mariam Haider <mfh.haider@gmail.com>
2020-04-09 21:32:33 +0000

    Automatically backed up by Learn

--- lib/get_requester.rb
+++ lib/get_requester.rb
@@ -3,6 +3,8 @@ require 'open-uri'
 
 class GetRequester
   
+  URL = "http://data.cityofnewyork.us/resource/uvks-tn5n.json"
+  
   def get_response_body 
     url = "https://learn-co-curriculum.github.io/json-site-example/"
   end 
