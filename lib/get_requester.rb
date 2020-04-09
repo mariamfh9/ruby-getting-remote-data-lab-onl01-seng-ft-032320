96866828bbe20b378fa646687ba601c373c630b1
Mariam Haider <mfh.haider@gmail.com>
2020-04-09 21:54:53 +0000

    Automatically backed up by Learn

--- lib/get_requester.rb
+++ lib/get_requester.rb
@@ -4,18 +4,20 @@ require 'json'
 
 class GetRequester
   
-  URL = "http://data.cityofnewyork.us/resource/uvks-tn5n.json"
+  #URL = "http://data.cityofnewyork.us/resource/uvks-tn5n.json"
   
   def get_response_body 
   #uri = URI.parse(URL)
   #response = Net::HTTP.get_response_body(uri)
   #response.body
+  
   end 
   
   def parse_json
     #programs = JSON.parse(self.get_response_body)
     #programs.collect do |program|
     #program["agency"]  
+    
     end
   end 
   
