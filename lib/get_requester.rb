470db1413b0910c1cafde332e2ca0f566de83249
Mariam Haider <mfh.haider@gmail.com>
2020-04-09 21:58:29 +0000

    Automatically backed up by Learn

--- lib/get_requester.rb
+++ lib/get_requester.rb
@@ -8,8 +8,8 @@ class GetRequester
   
   def get_response_body 
     uri = URI.parse(URL)
-  #response = Net::HTTP.get_response_body(uri)
-  #response.body
+    response = Net::HTTP.get_response(uri)
+    response.body
   
   end 
   
