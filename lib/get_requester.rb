79c1f8ac1c339fa6f39a609724b89f6a1d5e4e69
Mariam Haider <mfh.haider@gmail.com>
2020-04-09 22:20:31 +0000

    Automatically backed up by Learn

--- lib/get_requester.rb
+++ lib/get_requester.rb
@@ -18,13 +18,13 @@ class GetRequester
   
   end 
   
-  #def parse_json
-    #programs = JSON.parse(self.get_response_body)
+  def parse_json
+    programs = JSON.parse(self.get_response_body)
     #programs.collect do |program|
     #program["agency"]  
     
    #end
-  #end 
+  end 
   
   
 end 
